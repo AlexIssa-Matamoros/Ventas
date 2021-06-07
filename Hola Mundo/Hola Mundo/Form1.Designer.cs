8645ba54ebca3bd53f46bd14d434d9f6927ffe33
Issa Matt <isai.mat43@gmail.com>
2021-06-07 13:39:01 -0600

    Holaa

--- Hola Mundo/Hola Mundo/Form1.Designer.cs
+++ Hola Mundo/Hola Mundo/Form1.Designer.cs
@@ -47,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 149);
+            this.button1.Location = new System.Drawing.Point(75, 148);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(162, 63);
             this.button1.TabIndex = 1;
